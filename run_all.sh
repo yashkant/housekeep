0df90422907763ea614c0f62701ea11f5a7700b4
gchhablani <chhablani.gunjan@gmail.com>
2023-04-10 10:00:38 -0400

    Modify the run all script to ignore created dirs

--- run_all.sh
+++ run_all.sh
@@ -5,8 +5,14 @@ experiment="baseline_phasic_oracle"
 for file in /srv/flash1/gchhablani3/housekeep/logs/"$experiment"/configs/*.yaml
 do
     name=$(basename "$file" .yaml)
-    echo "Running command for $name..."
-    CSR_PATH="csr_raw/$name/$experiment"
-    export CSR_PATH=$CSR_PATH
-    ./run_cli.sh "$experiment" "$name" 100
+    echo "Checking if folder exists for $name..."
+    if [ -d "csr_raw/$name/$experiment" ]
+    then
+        echo "Folder already exists for $name, skipping..."
+    else
+        echo "Running command for $name..."
+        CSR_PATH="csr_raw/$name/$experiment"
+        export CSR_PATH=$CSR_PATH
+        ./run_cli.sh "$experiment" "$name" 50
+    fi
 done
