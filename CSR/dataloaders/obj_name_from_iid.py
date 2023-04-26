5555a64c611ef9d4419bc3d394c68c2e1a612a17
Kartik Venkata Ramachandruni <kvr6@sky1.cc.gatech.edu>
2023-04-25 23:38:14 -0400

    get obj name from iid (obsolete)

--- CSR/dataloaders/obj_name_from_iid.py
+++ CSR/dataloaders/obj_name_from_iid.py
@@ -5,8 +5,9 @@ from copy import deepcopy
 
 import torch
 
-scene = 'ihlen_0_int'
-source_dir = f'/srv/cvmlp-lab/flash1/gchhablani3/housekeep/csr_raw/{scene}/baseline_phasic_oracle/csr'
+scene = 'ihlen_1_int'
+assert os.path.exists(f'/srv/rail-lab/flash5/kvr6/dev/csr_full_final/{scene}')
+source_dir = f'/srv/cvmlp-lab/flash1/gchhablani3/housekeep/csr_raw/{scene}/baseline_phasic_oracle/observations'
 target_dir = f'/srv/rail-lab/flash5/kvr6/dev/csr_full_final'
 
 assert os.path.exists(source_dir), f'Path does not exist: {source_dir}'
@@ -20,28 +21,28 @@ inconsistent_iids = []
 
 for file in os.listdir(source_dir):
 
-    # print(f'Processing {file}...')
+    print(f'Processing {file}...')
 
     with open(os.path.join(source_dir, file), 'r') as fh:
-        data = json.load(fh) # keys: ['rgb', 'mask', 'depth', 'items', 'current_mapping', 'correct_mapping']
+        json_data = json.loads(json.load(fh)) # load observation file 
+        cos_eor_data = json_data[0]['cos_eor']
 
-        # '''     
-        # Task 1: Map iid (instance ids) to object/receptacle name (Note: multiple iids can point to one object class)
-        for item in data['items']: # keys: ['iid', 'sim_id', 'obj_key', 'type', 'bounding_box', 'cropped_image']
-            iid = item['iid']
-            objkey = item['obj_key']
+        def process_iid(any_iid):
+
+            any_sim_obj_id = cos_eor_data['iid_to_sim_obj_id'][str(any_iid)]
+            any_key = cos_eor_data['sim_obj_id_to_obj_key'][str(any_sim_obj_id)]
 
             # save iid to iid2objkey
-            if iid in iid2objkey.keys():
-                if not iid2objkey[iid] == objkey:
-                    if iid not in inconsistent_iids: inconsistent_iids.append(iid)
-                    print(f'{iid} | new: {objkey} and saved:{iid2objkey[iid]} are inconsistent!')
+            if any_iid in iid2objkey.keys():
+                if not iid2objkey[any_iid] == any_key:
+                    if any_iid not in inconsistent_iids: inconsistent_iids.append(any_iid)
+                    print(f'{any_iid} | new: {any_key} and saved:{iid2objkey[any_iid]} are inconsistent!')
 
             else:
-                iid2objkey[iid] = objkey
+                iid2objkey[any_iid] = any_key
 
-            if item['type'] == 'rec': # receptacle: storage_room_0-table_14_0.urdf
-                room_recep_compound = objkey.split('.')[0]
+            if '.urdf' in any_key: # receptacle: storage_room_0-table_14_0.urdf
+                room_recep_compound = any_key.split('.')[0]
 
                 room_indexed, recep_indexed = room_recep_compound.split('-')
 
@@ -50,41 +51,29 @@ for file in os.listdir(source_dir):
 
                 final_name = '_'.join(room_name_split) + '|' + '_'.join(recep_name_split)
 
-            elif item['type'] == 'obj': # object: condiment_1
+            else: # object: condiment_1
 
-                obj_name_split = [k for k in objkey.split('_') if not k.isdigit()]
+                any_name_split = [k for k in any_key.split('_') if not k.isdigit()]
 
-                final_name = '_'.join(obj_name_split)
+                final_name = '_'.join(any_name_split)
 
-            else:
-                raise KeyError('{} does not belong'.format(item['type']))
-
-            # save iid to iid2objname
-            if iid in iid2objname.keys():
-                if not iid2objname[iid] == final_name:
-                    if iid not in inconsistent_iids: inconsistent_iids.append(iid)
-                    print(f'{iid} | new: {final_name} and saved:{iid2objname[iid]} are inconsistent!')
+            # save any_iid to iid2objname
+            if any_iid in iid2objname.keys():
+                if not iid2objname[any_iid] == final_name:
+                    if any_iid not in inconsistent_iids: inconsistent_iids.append(any_iid)
+                    print(f'{any_iid} | new: {final_name} and saved:{iid2objname[any_iid]} are inconsistent!')
 
             else:
-                iid2objname[iid] = final_name
-        # '''
+                iid2objname[any_iid] = final_name
 
-        # Task 2: Map episode name to current mapping and correct mapping
-        #TODO: once iid issue is solved, save iid to iid mapping, not obj_key to obj_key mapping
-        episode_number = file.split('.')[0].split('_')[1]
-        assert episode_number.isdigit()
+        visible_obj_iids = json_data[0]['visible_obj_iids']
+        visible_rec_iids = json_data[0]['visible_recept_iids']
 
-        episode_object_mapping[f'{scene}|{episode_number}'] = dict({
-                'current_mapping': deepcopy(data['current_mapping']),
-                'correct_mapping': deepcopy(data['correct_mapping'])
-                })
-
-torch.save(dict({'iid_to_obj_key': iid2objkey,    
-                   'iid_to_obj_name': iid2objname,
-                   'scene_episode_to_object_mapping': episode_object_mapping,
-                   'inconsistent_iids': inconsistent_iids}), os.path.join(target_dir, 'master_iid_mapping.pkl'))
+        for iid_list in [visible_obj_iids, visible_rec_iids]:
+            for any_iid in iid_list:
+                process_iid(any_iid)
 
 
-# for k, v in iid2objname.items(): print(k, v)
-# input('wait')
-
+# torch.save(dict({'iid_to_obj_key': iid2objkey,    
+#                    'iid_to_obj_name': iid2objname,
+#                    'inconsistent_iids': inconsistent_iids}), os.path.join(target_dir, 'master_iid_mapping.pkl'))
